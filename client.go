cbb7b20129b690685041ccb5f728b6c2a42816b9
upton <updn@live.com>
2014-08-19 20:07:33 +0800

    add the client

---
+++ client.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+const (
+	AUTH       = "_A:"
+	HB         = "_H:"
+	MSG_PREFIX = "_M:"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "127.0.0.1:8888")
+	checkError(err)
+
+	conn.Write([]byte(AUTH + "upton"))
+	reader := bufio.NewReader(conn)
+
+	bb, _, err := reader.ReadLine()
+
+	fmt.Println("AUTH " + string(bb))
+
+	go hb(conn)
+
+	for {
+		bb, _, err := reader.ReadLine()
+		checkError(err)
+
+		fmt.Println("Server response:[" + string(bb) + "]")
+	}
+}
+
+func hb(conn net.Conn) {
+	for {
+		conn.Write([]byte(HB))
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
